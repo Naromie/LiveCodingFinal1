bd68641dd6876fa87cd4985da09b35cad2987d40
Naromie Simeon <naromie.simeon@gmail.com>
2020-02-04 19:27:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LiveCodingFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           public CalculateCandy numbercandy = new CalculateCandy();
+
+
+            Console.WriteLine("{ 0} The number of Candy you will need", NumberOFCandy);
+        }
+
+
+
+
+
+
+    }
+    public class int CalculateCandy(int NumberofTreater)
+    {
+
+        List<string> temperature =
+           new List<string> { "Clear", "Cloudy", "Raining", "Full Moon" };
+
+        int NumberOFCandy = 0;
+      
+        string weatherIndexer = "";
+        if (weatherIndexer == temperature[0])
+        {
+            NumberofTreater += NumberofTreater * 10 / 100;
+
+
+        }
+
+        else if (weatherIndexer == temperature[1])
+        {
+            NumberofTreater -= NumberofTreater * 25 / 100;
+        }
+
+
+        else if (weatherIndexer == temperature[2])
+        {
+            NumberofTreater += NumberofTreater * 25 / 100;
+        }
+
+        else if (weatherIndexer == temperature[3])
+        {
+            NumberofTreater += NumberofTreater * 25 / 100;
+
+        }
+    return NumberOFCandy;
+    }
+}
